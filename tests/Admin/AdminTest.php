900db6305db6c9e81265c3740c2dd810e733259d
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-03-14 14:16:11 +0100

    add tests

---
+++ tests/Admin/AdminTest.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.4
+ *
+ * @package   tests
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+namespace Modules\tests\Navigation\Admin;
+
+use Modules\Admin\Models\AccountMapper;
+use Modules\Navigation\Models\Navigation;
+use phpOMS\Message\Http\HttpRequest;
+use phpOMS\Module\ModuleManager;
+use phpOMS\Uri\HttpUri;
+
+/**
+ * @internal
+ */
+class AdminTest extends \PHPUnit\Framework\TestCase
+{
+    protected const MODULE_NAME = 'Navigation';
+    protected const URI_LOAD = '';
+
+    use \Modules\tests\ModuleTestTrait;
+
+    /**
+     * Test if navigation model works correct
+     *
+     * @group final
+     * @group module
+     */
+    public function testNavigationElements() : void
+    {
+        $request = new HttpRequest(new HttpUri('http://127.0.0.1/en/backend'));
+        $request->createRequestHashs(1);
+
+        $account       = AccountMapper::getWithPermissions(1);
+        $nav           = Navigation::getInstance($request, $account, $GLOBALS['dbpool'], 1, 'Backend')->getNav();
+        $moduleManager = new ModuleManager($this->app, __DIR__ . '/../../../../Modules');
+        $modules       = $moduleManager->getInstalledModules(false);
+
+        self::assertGreaterThan(0, \count($nav));
+    }
+}
---
+++ tests/Views/NavigationViewTest.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.4
+ *
+ * @package   tests
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+namespace Modules\tests\Navigation\Views;
+
+use Modules\Navigation\Views\NavigationView;
+
+/**
+ * @internal
+ */
+class NavigationViewTest extends \PHPUnit\Framework\TestCase
+{
+    public function testDefault() : void
+    {
+        $view = new NavigationView();
+
+        self::assertEquals(0, $view->getNavId());
+        self::assertEquals([], $view->getNav());
+        self::assertEquals(0, $view->getParent());
+    }
+
+    public function testGetSet() : void
+    {
+        $view = new NavigationView();
+
+        $view->setNavId(2);
+        self::assertEquals(2, $view->getNavId());
+
+        $view->setNav([1, 2, 3]);
+        self::assertEquals([1, 2, 3], $view->getNav());
+
+        $view->setParent(4);
+        self::assertEquals(4, $view->getParent());
+    }
+}
