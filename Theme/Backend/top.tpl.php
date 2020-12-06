c2f8c5481366e2e1ba12581ad1ad2781d29e7091
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-12-06 11:13:19 +0100

    tpl fixes, dont use printHtml for none-string types

--- Theme/Backend/top.tpl.php
+++ Theme/Backend/top.tpl.php
@@ -25,7 +25,7 @@ if (isset($this->nav[NavigationType::TOP])) : ?>
         <li><a id="link-<?= $id; ?>" target="<?= $link['nav_target']; ?>" href="<?= \phpOMS\Uri\UriFactory::build($link['nav_uri']); ?>"<?= $link['nav_action'] !== null ? ' data-action=\'' . $link['nav_action'] . '\'' : ''; ?>>
 
                 <?php if (isset($link['nav_icon'])) : ?>
-                    <i class="<?= $this->printHtml($link['nav_icon']); ?> infoIcon"><?php if (isset($unread[$link['nav_from']]) && $unread[$link['nav_from']] > 0) : ?><span class="badge"><?= $this->printHtml($unread[$link['nav_from']]); ?></span><?php endif; ?></i>
+                    <i class="<?= $this->printHtml($link['nav_icon']); ?> infoIcon"><?php if (isset($unread[$link['nav_from']]) && $unread[$link['nav_from']] > 0) : ?><span class="badge"><?= $unread[$link['nav_from']]; ?></span><?php endif; ?></i>
                 <?php endif; ?>
 
                 <span class="link"><?= $this->getHtml($link['nav_name'], 'Navigation'); ?><span></a>
