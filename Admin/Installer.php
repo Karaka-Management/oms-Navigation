eb7cd76a113c4f6ec8e6b48a0a2c323686f04e9e
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-10-03 09:16:47 +0200

    phpcs, static and unit test fixes

--- Admin/Installer.php
+++ Admin/Installer.php
@@ -32,6 +32,14 @@ use phpOMS\System\File\PathException;
 final class Installer extends InstallerAbstract
 {
     /**
+     * Path of the file
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    public const PATH = __DIR__;
+
+    /**
      * Install data from providing modules.
      *
      * @param ApplicationAbstract $app  Application
