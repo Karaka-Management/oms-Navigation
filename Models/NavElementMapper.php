035d4f33a1369cd8a24e81c8a41aa8bbd4552547
Dennis Eichhorn <spl1nes.com@googlemail.com>
2023-04-11 00:20:12 +0200

    fix static analysis

--- Models/NavElementMapper.php
+++ Models/NavElementMapper.php
@@ -23,6 +23,9 @@ use phpOMS\DataStorage\Database\Mapper\DataMapperFactory;
  * @license OMS License 2.0
  * @link    https://jingga.app
  * @since   1.0.0
+ *
+ * @template T of NavElement
+ * @extends DataMapperFactory<T>
  */
 final class NavElementMapper extends DataMapperFactory
 {
@@ -56,7 +59,7 @@ final class NavElementMapper extends DataMapperFactory
     /**
      * Model to use by the mapper.
      *
-     * @var class-string
+     * @var class-string<T>
      * @since 1.0.0
      */
     public const MODEL = NavElement::class;
