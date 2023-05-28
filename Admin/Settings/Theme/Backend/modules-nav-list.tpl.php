6846357924dea206aad766664b0697509011be9f
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-05-28 01:13:22 +0000

    style fixes, bug fixes

--- Admin/Settings/Theme/Backend/modules-nav-list.tpl.php
+++ Admin/Settings/Theme/Backend/modules-nav-list.tpl.php
@@ -24,7 +24,7 @@ echo $this->getData('nav')->render();
 <div class="row">
     <div class="col-xs-12">
         <div class="portlet">
-            <div class="portlet-head"><?= $this->getHtml('Navigation'); ?><i class="fa fa-download floatRight download btn"></i></div>
+            <div class="portlet-head"><?= $this->getHtml('Navigation'); ?><i class="lni lni-download download btn end-xs"></i></div>
             <div class="slider">
             <table id="navElements" class="default sticky">
                 <thead>
