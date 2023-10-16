4f02fb89a21946eb3fb92c72d7063aa9275f25f1
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-10-16 23:39:39 +0000

    Add test cases for null models

--- tests/Models/NullNavElementTest.php
+++ tests/Models/NullNavElementTest.php
@@ -23,7 +23,7 @@ final class NullNavElementTest extends \PHPUnit\Framework\TestCase
 {
     /**
      * @covers Modules\Navigation\Models\NullNavElement
-     * @group framework
+     * @group module
      */
     public function testNull() : void
     {
@@ -32,7 +32,7 @@ final class NullNavElementTest extends \PHPUnit\Framework\TestCase
 
     /**
      * @covers Modules\Navigation\Models\NullNavElement
-     * @group framework
+     * @group module
      */
     public function testId() : void
     {
@@ -42,7 +42,7 @@ final class NullNavElementTest extends \PHPUnit\Framework\TestCase
 
     /**
      * @covers Modules\Navigation\Models\NullNavElement
-     * @group framework
+     * @group module
      */
     public function testJsonSerialize() : void
     {
