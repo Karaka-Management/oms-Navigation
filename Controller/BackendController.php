43fe7714f5090da69a24fcc29b24d491d4f2a935
Dennis Eichhorn <spl1nes.com@googlemail.com>
2024-05-21 00:09:14 +0200

    fix version and bugs

--- Controller/BackendController.php
+++ Controller/BackendController.php
@@ -6,7 +6,7 @@
  *
  * @package   Modules\Navigation
  * @copyright Dennis Eichhorn
- * @license   OMS License 2.0
+ * @license   OMS License 2.2
  * @version   1.0.0
  * @link      https://jingga.app
  */
@@ -28,7 +28,7 @@ use phpOMS\Views\View;
  * Navigation class.
  *
  * @package Modules\Navigation
- * @license OMS License 2.0
+ * @license OMS License 2.2
  * @link    https://jingga.app
  * @since   1.0.0
  * @codeCoverageIgnore
--- Models/NullNavElement.php
+++ Models/NullNavElement.php
@@ -6,7 +6,7 @@
  *
  * @package   Modules\Navigation\Models
  * @copyright Dennis Eichhorn
- * @license   OMS License 2.0
+ * @license   OMS License 2.2
  * @version   1.0.0
  * @link      https://jingga.app
  */
@@ -18,7 +18,7 @@ namespace Modules\Navigation\Models;
  * Null model
  *
  * @package Modules\Navigation\Models
- * @license OMS License 2.0
+ * @license OMS License 2.2
  * @link    https://jingga.app
  * @since   1.0.0
  */
